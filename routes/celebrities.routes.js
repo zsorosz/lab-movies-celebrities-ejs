f89b6141ee4943b01c39e00242ca92d02b3615a9
zsorosz <oroszi.zsofi@gmail.com>
2023-02-06 16:45:58 +0100

    Iteration-3 done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,7 +1,19 @@
+const CelebrityModel = require("../models/Celebrity.model");
+
 const router = require("express").Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
+  res.render("celebrities/celebrities");
+});
+
+router.get("/create", (req, res) => {
+  res.render("celebrities/new-celebrity");
+});
+
+router.post("/create", async (req, res) => {
+  const newCeleb = req.body;
+  await CelebrityModel.create(newCeleb);
   res.render("celebrities");
 });
 
